50a3587621a3c2ea241b9e351ca12018bc9738d1
walidlag <walidlagmah8@gmail.com>
2024-02-29 12:00:19 -0800

    wall

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,16 +10,16 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new;
+    binary_tree_t *new_node;
 
-	new = malloc(sizeof(binary_tree_t));
-	if (new == NULL)
-		return (NULL);
+    new_node = malloc(sizeof(binary_tree_t));
+    if (!new_node)
+        return (NULL);
 
-	new->n = value;
-	new->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
+    new_node->parent = parent;
+    new_node->value = value;
+    new_node->left = NULL;
+    new_node->right = NULL;
 
-	return (new);
+    return (new_node);
 }
